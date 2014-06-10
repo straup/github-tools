e53129bd1c5cb11e59ba91709acfe977129f38a8
straup <straup@gmail.com>
2014-06-10 08:46:00 -0400

    make work with personal-api-tokens; use requests.py instead of urllib

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 A 5-minute script to clone/pull all a user's (public) repos (in read-only mode) to a local directory. Needs auth-y hooks for private repositories. This is basically just a safe guard and has no bells. Or whistles.
 
-	$> clone-repos.py <username> </path/to/github-clone> [ list of repos to skip ]
+	$> clone-repos.py --token <personal-api-token> --outdir </path/to/github-clone> [ list of repos to skip ]
+
+### See also
+
+* https://github.com/blog/1509-personal-api-tokens
--- bin/clone-repos.py
+++ bin/clone-repos.py
@@ -3,106 +3,132 @@
 import os
 import os.path
 import json
-import urllib
+import urllib2
 import logging
 import subprocess
 import types
+import requests
 
-def clone_repo(repo, outdir):
-
-    path = os.path.join(outdir, repo['name'])
-    logging.info("clone %s to %s" % (repo['name'], path))
-
-    if os.path.isdir(path):
-
-        os.chdir(path)
-
-        args = [
-            "git",
-            "pull",
-            "origin",
-            "master"
-            ]
-
-    else:
-
-        args = [
-            "git",
-            "clone",
-            repo['git_url'],
-            path
-            ]
-
-    rsp = subprocess.check_call(args)
-    logging.debug(rsp)
+class gethub:
     
-def clone_list(user, outdir, skip=[]):
+    def __init__(self):
+        pass
 
-    # because, stupid... so insanely stupid
-    # https://developer.github.com/v3/#pagination
+    def clone_repo(self, repo, outdir):
 
-    next = "https://api.github.com/users/%s/repos" % user
+        path = os.path.join(outdir, repo['name'])
+        logging.info("clone %s to %s" % (repo['name'], path))
+        
+        if os.path.isdir(path):
 
-    while next:
+            os.chdir(path)
 
-        logging.debug("fetch %s" % next)
+            args = [
+                "git",
+                "pull",
+                "origin",
+                "master"
+            ]
 
-        try:
-            rsp = urllib.urlopen(next)
-        except Exception, e:
-            logging.error("Failed to retrieve %s, because %s" % (next, e))
-            return False
+        else:
 
-        for k, v in rsp.info().items():
+            args = [
+                "git",
+                "clone",
+                repo['git_url'],
+                path
+            ]
 
-            if k == 'link':
+            rsp = subprocess.check_call(args)
+            logging.debug(rsp)
 
-                links = {}
-                rels = v.split(', ')
+    # to do: clone_organization
+
+    def clone_user(self, token, outdir, skip=[]):
+
+        next = "https://api.github.com/user/repos"
+
+        while next:
+            
+            url = next
+            logging.debug("fetch %s" % url)
+            
+            try:
+                rsp = requests.get(url, auth=(token, ''), stream=True)
+            except Exception, e:
+                logging.error("Failed to retrieve %s, because %s" % (url, e))
+                return False
                 
-                for rel in rels:
-                    rel = rel.split('; ')
-                    links[rel[1]] = rel[0]
-                    
-                    next = links.get('rel="next"', None)
+            # Seriously... this is better than plain vanilla pagination?
+                
+            headers = rsp.headers
+            link = headers.get('link', None)
+            
+            if not link:
+                logging.error("Can't find 'link' header")
+                return False
+                
+            rels = link.split(', ')
+            links = {}
+
+            for rel in rels:
+                rel = rel.split('; ')
+                links[rel[1]] = rel[0]
+                
+                next = links.get('rel="next"', None)
+
+                if next:
+                    next = next.replace("<", "")
+                    next = next.replace(">", "")
                     logging.debug("next is %s" % next)
-                    break
 
                 break
 
-        try:
-            data = json.load(rsp)
-        except Exception, e:
-            logging.error("Failed to parse %s, because %s" % (next, e))
-            return False
-        
-        for repo in data:
-
-            if not type(repo) == types.DictType:
-                print "WTF is %s" % repo
-                continue
-
-            name = repo.get('name', None)
-
-            if not name:
-                continue
-
-            if name in skip:
-                continue
-
             try:
-                clone_repo(repo, outdir)
+                data = rsp.json()
             except Exception, e:
-                logging.error(e)
+                logging.error("Failed to parse %s, because %s" % (url, e))
+                return False
+        
+            for repo in data:
+
+                name = repo.get('name', None)
+
+                if not name:
+                    continue
+                
+                if name in skip:
+                    continue
+
+                try:
+                    self.clone_repo(repo, outdir)
+                except Exception, e:
+                    logging.error(e)
 
 
 if __name__ == '__main__':
 
-    import sys
-    user = sys.argv[1]
-    outdir = sys.argv[2]
-    skip = sys.argv[3:]
+    import optparse
+    parser = optparse.OptionParser()
 
-    clone_list(user, outdir, skip)
+    # sudo read from config file...
+
+    parser.add_option('--token', dest='token', action='store', help='')
+    parser.add_option('--outdir', dest='outdir', action='store', help='')
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="enable chatty logging; default is false", default=False)
+
+    (opts, args) = parser.parse_args()
+
+    if opts.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if not os.path.isdir(opts.outdir):
+        logging.error("%s is not a directory" % opts.outdir)
+        sys.exit()
+
+    gh = gethub()
+    gh.clone_user(opts.token, opts.outdir, args)
 
 
